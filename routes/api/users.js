b94ce294858183b7b5a2e679d1852f513eef6b18
Fakhri Albari <fakhri.albari@ecomindo.com>
2022-08-31 21:11:29 +0700

    init validation for user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route   GET api/users
+const arrValidator = [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').not().isEmpty(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+];
+
+// @route   POST api/users
 // @desc    TEST route
 // @access  Public
-router.get('/', (req, res) => res.send('User Route'));
+router.post('/', arrValidator, (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+    console.log(req.body);
+    res.send('User Route')
+});
 
 module.exports = router;
