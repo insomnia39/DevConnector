36b59dd7c1778c87263dc89a8b111ae136fd4058
Fakhri Albari <fakhri.albari@ecomindo.com>
2022-08-31 21:42:30 +0700

    add user registration logic

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
 
 const arrValidator = [
     check('name', 'Name is required').not().isEmpty(),
@@ -8,16 +12,33 @@ const arrValidator = [
     check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
 ];
 
+async function userExistValidation(email, res){
+    const user = await User.findOne({ email });
+    if(user){
+        res.status(400).json({ errors: [{ msg: 'User already exists'}]});
+    }
+}
+
 // @route   POST api/users
 // @desc    TEST route
 // @access  Public
-router.post('/', arrValidator, (req, res) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({ errors: errors.array() });
+router.post('/', arrValidator, async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const { name, email, password } = req.body;
+        await userExistValidation(email, res);
+        const avatar = gravatar.url(email, { s: '200', r: 'pg', d: 'mm'});
+        const user = new User({name, email, avatar, password});
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+        user.save();
+        res.send(`User registered. Id : ${user.id}`);
+    } catch (error) {
+        res.status(500).send('Server Error');
     }
-    console.log(req.body);
-    res.send('User Route')
 });
 
 module.exports = router;
